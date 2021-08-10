6644b4fd2d5c54eed8d9b6e6eb2aca06f55333ad
Ian Anderson <i.github@operation-sensory-overload.com>
2021-08-10 12:55:17 -0400

    getlogs: Fill out skeleton. Add binary to ignore list.
    -Remove test code
    -Add TODOs for various command line flags
    -Quit unless running as root (geteuid = 0)
    -Quit if we failed to connect to the database

--- getlogs/getlogs.go
+++ getlogs/getlogs.go
@@ -3,18 +3,40 @@ package main
 import (
 	"context"
 	"fmt"
-	"os/user"
-
 	"github.com/jackc/pgx/v4"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello Go!")
-	CurrentUser, _ := user.Current()
-	fmt.Println("Current user is", CurrentUser.Name)
+	// TODO: Command line flags
+	// TODO: Add debug flag
+	// TODO: Add flags for specifying universe name or UUID
+	// TODO: Add flags to control node list source(s) (platform postgres, YB masters, or manual)
+	// TODO: Add flags for specifying individual nodes by number and by name
+	// TODO: Add flags for controlling time window (before / after)
+	// TODO: Add flags for controlling which logs to collect (info/error/fatal)
+
+	Euid := os.Geteuid()
+	if Euid != 0 {
+		// fmt.Println("Effective user id: ", Euid)
+
+		_, _ = fmt.Fprintln(os.Stderr, "The getlogs utility must be run with root privileges.")
+		os.Exit(1)
+	}
+
 	// TODO: Replace hard-coded URL
 	// DbUrl := os.Getenv("DATABASE_URL")
 	DbUrl := "postgres://postgres:GwxSSDZcyUHVY8g8@localhost:5432/yugaware"
-	conn, _ := pgx.Connect(context.Background(), DbUrl)
-	fmt.Println("Connection info:", conn)
+
+	fmt.Println("Connecting to Yugaware database to retrieve node info")
+	conn, err := pgx.Connect(context.Background(), DbUrl)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+	fmt.Println("Connected to Yugaware database successfully")
+	// fmt.Println("Connection info:", conn)
+
+	// TODO: Retrieve universe JSON from postgres
 }
